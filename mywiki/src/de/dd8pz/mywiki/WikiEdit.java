ae7d5d1f78af4dcdfaea683ab978447a68d75363
Jochen van Geldern <dd8pz@imail.de>
2012-10-05 16:34:23 +0200

    Dialog EInstellungen gestaltet.

--- mywiki/src/de/dd8pz/mywiki/WikiEdit.java
+++ mywiki/src/de/dd8pz/mywiki/WikiEdit.java
@@ -3,18 +3,29 @@ package de.dd8pz.mywiki;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.widget.EditText;
 
 public class WikiEdit extends Activity {
 
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiki_edit);
+        LoadPage();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_wiki_edit, menu);
         return true;
+    }
+    
+    public void LoadPage() {
+        WebView webView1 = (WebView) findViewById(R.id.webView1);
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        mySettings.loginSite();
+        webView1.getSettings().setJavaScriptEnabled(true);
+        webView1.loadUrl(mySetting.getUrl+editText1.getText);
     }
 }
